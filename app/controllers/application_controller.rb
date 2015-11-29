3cec4f27e0b3623e482e388861a19d798e729ccd
Tracey Carter <traceycarterdbc@gmail.com>
2015-11-28 16:04:31 -0800

    change database to postgres for heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+     render text: "hello, world! This is the San Francisco Family Support Network overhaul of existing systems."
+  end
 end
